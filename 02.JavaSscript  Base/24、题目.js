1dd57cefb3e386258c94b5736f18f45db77673e9
符欢梅 <3533643117@qq.com>
2024-10-02 12:26:43 +0800

    learning

--- 02.JavaSscript  Base/24、题目.js
+++ 02.JavaSscript  Base/24、题目.js
@@ -18,10 +18,80 @@
 // };
 // console.log(i);//11
 
-//题目4：眼神有问题
-for(var i = 1;i <= 5;i++){
-    if(i > 3){
-        continue;//4 5停止执行
-    }
-    console.log(i); //1 2 3 
+//题目4：
+// for(var i = 1;i <= 5;i++){
+//     if(i > 3){
+//         continue;//4 5停止执行
+//     }
+//     console.log(i); //1 2 3 
+// };
+
+//题目5：能被3或7整除的100到200以内的整数
+// let arr = [];
+// for(var i = 100;i<=200;i++){
+//     if(i % 3 === 0 || i % 7 ===0){
+//         // console.log(i);
+//         arr.push(i);
+//         // console.count();
+//     };
+// };
+// console.log(arr);
+/*
+[
+  102, 105, 108, 111, 112, 114, 117,
+  119, 120, 123, 126, 129, 132, 133,
+  135, 138, 140, 141, 144, 147, 150,
+  153, 154, 156, 159, 161, 162, 165,
+  168, 171, 174, 175, 177, 180, 182,
+  183, 186, 189, 192, 195, 196, 198
+] */
+
+//题目6：位上有3或7的3位数【以目前学习的方法还没啥思路】
+//先找到三位数，100~999
+//关键是分别取出个位数、十位数、百位数
+//103 113
+//107 117
+// var arr = [];
+// for(var num = 100;num<=999;num++){
+//     //百位
+//     var bai = parseInt(num / 100);
+//     //十位
+//     var shi = parseInt((num - bai * 100) / 10);
+//     //个位
+//     var ge = num - bai * 100 - shi * 10;
+//     if(bai === 3 || bai === 7 ){
+//         // console.log(num);
+//         arr.push(num);
+//     }else if(shi === 3 || shi === 7){
+//         // console.log(num);
+//         arr.push(num);
+//     }else if(ge === 3 || ge === 7){
+//         // console.log(num);
+//         arr.push(num);
+//     };
+// };
+// console.log(arr);
+
+// //第2种方法：获取百位数、十位数、个位数
+// var n1 = parseInt(num % 100);
+// var n2 = parseInt(num % 100 / 10);
+// var n3 = num % 10;
+
+
+//题目7：20阶乘的和
+var res = 1;
+var sum = 0;
+for(var i = 1;i<=20;i++){
+   res = res * i;//其实我这个有点儿绕弯路了！！！
+   sum = sum + res
 };
+console.log("20的阶乘res=",res);//2432902008176640000
+console.log("20的阶乘的和sum=",sum);//2561327494111820300
+//使用1层循环实现
+var sum1 =0,res1=1;
+//循环从1~20
+for(var n = 1; n <= 20; n++){
+    res1 *= n;
+    sum1 += res1;
+};
+console.log( res1,sum1);//2432902008176640000 2561327494111820300
---
+++ 02.JavaSscript  Base/26、创建数组的方式.js
@@ -0,0 +1,76 @@
+// //创建方式1：直接量
+// var arr1 = [1,2,'hello',true,null,undefined];
+// console.log(arr1,typeof arr1);//[ 1, 2, 'hello', true, null, undefined ] object
+
+// //创建方式2：Array()函数
+// var arr2 = Array(1,2,'hello',true,null);
+// console.log(arr2,typeof arr2);//[ 1, 2, 'hello', true, null ] object
+// //1个参数且是数值类型：表示有17个元素，但都是空数组，但占位
+// var arr2 = Array(17);
+// console.log(arr2);//[ <17 empty items> ]
+
+// //创建方式3：new Array();
+// console.log(new Array(1,2));//[ 1, 2 ]
+
+// const arr3 = [1,2,"hello world",true,false,undefined,null,NaN,[1,2,3,4,5]];
+// //读取元素
+// console.log(arr3[3]+arr3[5]);//NaN
+// console.log(arr3[8][2]);//3
+// console.log(arr3[9]);//使用数组中不存在的元素会得到一个undefined
+
+// //修改数组中的元素的值
+// arr3[2] = "快乐";
+// console.log(arr3);//[ 1, 2, '快乐', true, false, undefined, null, NaN, [ 1, 2, 3, 4, 5 ] ]
+// //给不存在的索引赋值，直接放到该数组夫人最后位置即可
+// arr3[10] = 123;
+// console.log(arr3);
+
+// //稀疏数组
+// var arr4 = [100,0,990,Array(6)];
+// console.log(arr4);//[ 100, 0, 990, [ <6 empty items> ] ]
+
+//题目1：
+//创建数组
+// var nums = [34,101,28,865,341,106,228,199];
+// //计算数组中所有元素的和
+// var sum = 0;
+// //计算数组的元素的平均数
+// var evenNum = 0
+
+// for(var i = 0;i<nums.length;i++){
+//     sum += nums[i];
+//     evenNum = sum / nums.length;
+    
+// };
+// console.log(sum);//1902
+// console.log(evenNum);//237.75
+
+// var nums = [34,101,28,865,341,106,228,199];
+// //最大值【没啥思路】
+// var maxNum = nums[0];
+// for(var i=1;i<nums.length;i++){
+//     //如果当前遍历到的元素比maxNum大，修改maxNum的值为当前元素
+//     if(maxNum < nums[i]){
+//         maxNum = nums[i];
+//     };
+// };
+// console.log("数组中最大的元素maxNum=",maxNum);
+
+// var min = nums[0];
+// for(var i=1;i<nums.length;i++){
+//     //如果当前遍历到的元素比maxNum大，修改maxNum的值为当前元素
+//     if(min > nums[i]){
+//         min = nums[i];
+//     };
+// };
+// console.log("数组中最大的元素min=",min);//数组中最大的元素min= 28
+
+var nameList = ["张安","李四","张飞","李逵","松松","小鬼"];
+nameList[nameList.length] = "小娟";
+// console.log(nameList);
+//push()添加到数组的最后
+nameList.push("张张");
+console.log(nameList);
+//unshift()添加到数组的前面
+nameList.unshift("第一","第二");
+console.log(nameList);
